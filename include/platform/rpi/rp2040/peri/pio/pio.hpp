f01e7b269e248eb93498223bde50033a67e05749
Skyler Mansfield <90399509+Skyler84@users.noreply.github.com>
2025-03-06 16:08:48 +0000

    PIO: exec_instr should be completed now!

--- include/platform/rpi/rp2040/peri/pio/pio.hpp
+++ include/platform/rpi/rp2040/peri/pio/pio.hpp
@@ -66,12 +66,13 @@ namespace RP2040::PIO{
       uint8_t hi5 =  (instr>>8)&0x1f;
       uint16_t nextPC;
       bool was_stalled = m_stall;
-      if (m_stall) {
+      bool was_exec = m_instr_exec;
+      if (was_stalled || was_exec) {
         nextPC = m_pc;
       }else if (m_pc == m_wrap_top) {
         nextPC = m_wrap_bottom;
       } else {
-        nextPC = m_pc+1;
+        nextPC = (m_pc+1) % 32;
       }
       switch(instr & 0xe000) {
         case 0x0000: // JMP
@@ -111,10 +112,7 @@ namespace RP2040::PIO{
               break;
             case 0b10: // IRQ
             {
-              uint8_t irqNum = lo5 & 0x07;
-              if (lo5 & 0x10) {
-                irqNum = irqNum & 0x04 + (irqNum + m_id) & 3;
-              }
+              uint8_t irqNum = irq_decode(lo5);
               auto &irq = m_block.m_irqs[irqNum];
               m_stall = irq.operator bool() != polarity;
               if (polarity)
@@ -195,7 +193,7 @@ namespace RP2040::PIO{
             switch (mid3) {
               case 0b000: // PINS
               {
-                for (int i = 0; i < BitCnt; i++) {
+                for (int i = 0; i < m_out_count; i++) {
                   m_block.m_gpio[(i+m_out_base)%32].set_output(bits_out & 1);
                   bits_out >>= 1;
                 }
@@ -208,7 +206,7 @@ namespace RP2040::PIO{
                 break;
               case 0b100: // PINDIRS
               {
-                for (int i = 0; i < BitCnt; i++) {
+                for (int i = 0; i < m_out_count; i++) {
                   m_block.m_gpio[(i+m_out_base)%32].set_oe(bits_out & 1);
                   bits_out >>= 1;
                 }
@@ -239,83 +237,171 @@ namespace RP2040::PIO{
         } break;
         case 0x8000: // PUSH/PULL
         {
-          bool cond1 = (mid3&4)?(m_output_shift_cnt >= m_pull_threshold):(m_input_shift_cnt >= m_push_threshold);
+          bool cond1 = (mid3&4)?(m_output_shift_cnt < m_pull_threshold):(m_input_shift_cnt < m_push_threshold);
           bool cond2 = (mid3&4)?m_tx_fifo.empty():m_rx_fifo.full();
-          if 
-          switch (mid3) {
-            case 0b000: // PUSH, ALWAYS, NOBLOCK
-            if (!m_rx_fifo.full()) {
-              m_rx_fifo.push(m_isr);
-              m_isr = 0;
-              m_input_shift_cnt = 0;
-              m_stall = false;
-            } break;
-            case 0x001: // PUSH, ALWAYS, BLOCK
-            {
-              if (m_rx_fifo.full()) {
-                m_stall = true;
-              } else {
-                m_rx_fifo.push(m_isr);
-                m_isr = 0;
-                m_input_shift_cnt = 0;
-                m_stall = false;
-              }
-            } break;
-            case 0x010: // PUSH, IFFULL, NOBLOCK
-            {
-              if (m_input_shift_cnt >= m_push_threshold && !m_rx_fifo.full()) {
-                m_rx_fifo.push(m_isr);
-                m_isr = 0;
-                m_input_shift_cnt = 0;
-                m_stall = false;
-              }
-            } break;
-            case 0x011: // PUSH, IFFULL, BLOCK
-            {
-              if (m_input_shift_cnt >= m_push_threshold) {
-                if (m_rx_fifo.full()) {
-                  m_stall = true;
-                } else {
-                  m_rx_fifo.push(m_isr);
-                  m_isr = 0;
-                  m_input_shift_cnt = 0;
-                  m_stall = false;
-                }
-              }
-            } break;
-            case 0x100: // PULL, ALWAYS, NOBLOCK
-            {
-              if (!m_tx_fifo.empty()) {
-                m_osr = m_tx_fifo.pop();
-                m_output_shift_cnt = 0;
-                m_stall = false;
-              }
-            } break;
-            case 0x101: // PULL, ALWAYS, BLOCK
-            {
-              if (m_tx_fifo.empty()) {
-                m_stall = true;
-              } else {
-                m_osr = m_tx_fifo.pop();
-                m_output_shift_cnt = 0;
-                m_stall = false;
-              }
-            } break;
-            case 0x110: // PULL, IFEMPTY, NOBLOCK
-            {
-              if (m_output_shift_cnt >= m_pull_threshold && !m_tx_fifo.empty()) {
-                m_osr = m_tx_fifo.pop();
-                m_output_shift_cnt = 0;
-                m_stall = false;
-              } 
-            } break;
-            case 0x111: // PULL, IFEMPTY, BLOCK
-
+          if (mid3 & 2 && cond1) {
+            // skip
+          } else if (mid3 & 1 && cond2) {
+            // block
+            stall = true;
+          } else if (mid3 & 4) {
+            // pull
+            m_osr = m_tx_fifo.pop();
+            m_output_shift_cnt = 0;
+            m_stall = false;
+          } else {
+            // push
+            m_rx_fifo.push(m_isr);
+            m_isr = 0;
+            m_input_shift_cnt = 0;
+            m_stall = false;
           }
         } break;
         case 0xa000: // MOV
+        {
+          uint8_t src = lo5 & 7;
+          uint8_t dst = mid3;
+          uint8_t op = lo5 >> 3;
+          uint32_t data;
+          switch (src) {
+            case 0b000: // PINS
+            {
+              for (int i = 0; i < 32; i++) {
+                data |= m_block.m_gpio[(i+m_in_base)%32].get_input() << i;
+              }
+            } break;
+            case 0b001: // X
+              data = m_scratch_x;
+              break;
+            case 0b010: // Y
+              data = m_scratch_y;
+              break;
+            case 0b011: // NULL
+              data = 0;
+              break;
+            case 0b100: // Reserved
+              break;
+            case 0b101: // STATUS
+              data = status();
+              break;
+            case 0b110: // ISR
+              data = m_isr;
+              break;
+            case 0b111: // OSR
+              data = m_osr;
+              break;
+          }
+          switch (op) {
+            case 0b00: // NOOP
+              break;
+            case 0b01: // INVERT
+              data = ~data;
+              break;
+            case 0b10: // BIT-REV
+            {
+              uint32_t mask = 0xffffffff;
+              // fast bitrev algo
+              // O(log(n)) execution time vs
+              // naive single bitshift implementation which takes O(n) time
+              // this does assume that shifting is constant time independant of 
+              // shift count
+              for (uint8_t i = 16; i; i>>=1) {
+                mask ^= mask >> i;
+                data = ((data & mask) >> i) | ((data & ~mask) << i);
+              }
+            } break;
+            case 0b11: // Reserved
+          }
+          switch (dst) {
+            case 0b000: // PINS
+            {
+              for (int i = 0; i < m_out_count; i++) {
+                m_block.m_gpio[(i+m_out_base)%32].set_output(data & i);
+                data >>= 1;
+              }
+            } break;
+            case 0b001: // X
+              m_scratch_x = data;
+              break;
+            case 0b010: // Y
+              m_scratch_y = data;
+              break;
+            case 0b011: // Reserved
+              break;
+            case 0b100: // EXEC
+              m_exec_instr = data;
+              m_instr_exec = true;
+              break;
+            case 0b101: // PC
+              nextPC = data;
+              break;
+            case 0b110: // ISR
+              m_isr = data;
+              m_input_shift_cnt = 0;
+              break;
+            case 0b111: // OSR
+              m_osr = data;
+              m_output_shift_cnt = 0;
+              break;
+          }
+        } break;
         case 0xc000: // IRQ
+        {
+          uint8_t irqNum = irq_decode(lo5);
+          auto &irq = m_block.m_irqs[irqNum];
+          switch (mid3) {
+            case 0b000: // SET, NOWAIT
+              irq.raise();
+              break;
+            case 0b001: // SET, WAIT
+              if (!was_stalled) {
+                irq.raise();
+                m_stall = true;
+              }
+              if (!irq) {
+                m_stall = false;
+              }
+              break;
+            case 0b010: // CLR, NOWAIT
+            case 0b011: // CLR, WAIT?
+              irq.lower();
+              break;
+            default:
+              break;
+          }
+        } break;
         case 0xe000: // SET
+        {
+          switch (mid3) {
+            case 0b000: // PINS
+            {
+              for (int i = 0; i < m_set_count; i++) {
+                m_block.m_gpio[(i+m_set_base)%32].set_output(lo5 & 1);
+                lo5 >>= 1;
+              }
+            } break;
+            case 0b001: // X
+              m_scratch_x = lo5;
+              break;
+            case 0b010: // Y
+              m_scratch_y = lo5;
+              break;
+            case 0b101: // Reserved
+            case 0b011: // Reserved
+              break;
+            case 0b100: // PINDIRS
+            {
+              for (int i = 0; i < m_set_count; i++) {
+                m_block.m_gpio[(i+m_set_base)%32].set_oe(lo5 & 1);
+                lo5 >>= 1;
+              }
+            } break;
+            case 0b101: // Reserved
+            case 0b110: // Reserved
+            case 0b111: // Reserved
+              break;
+          }
+        } break;
       }
       // handle side-set
       // only if we were un-stalled (ie first execution of instruction only!)
@@ -333,8 +419,32 @@ namespace RP2040::PIO{
       }
 
       m_pc = nextPC;
+      uint8_t delay_bits = 5 - m_sideset_count;
+      m_delay_cycles = hi5 >> m_sideset_count;
     }
-    void tick();
+    void tick()
+    {
+      uint8_t instr;
+      // execute forced/exec'd instructions immediately
+      // skips OUT EXEC delay cycles!
+      if (m_instr_exec) {
+        instr = m_exec_instr;
+      // } else if ({
+      // if we're stalled or we don't have delay cycles, just execute the instruction
+      } else if (!m_stall && m_delay_cycles) {
+        m_delay_cycles--;
+        return;
+      } else {
+        if (!m_stall) {
+          m_instr = m_block.instrmem()[m_pc];
+        }
+        instr = m_instr;
+      }
+      exec_instr(m_instr);
+      if (!m_stall) {
+        m_instr_exec = false;
+      }
+    }
     uint32_t pinctrl() const { 
       return 
           m_out_base << 0
@@ -358,11 +468,28 @@ namespace RP2040::PIO{
     uint32_t shiftctrl() const { return m_isr | (m_osr << 16); }
     const PIOTxFifo &tx_fifo() const { return m_tx_fifo; }
     const PIORxFifo &rx_fifo() const { return m_rx_fifo; }
-    bool osre() const { }
+    bool osre() const { return m_output_shift_cnt >= m_pull_threshold; }
     bool autopush() const { return m_autopush; }
     bool autopull() const { return m_autopull; }
     // auto &gpios() { return m_block.gpio; }
+    uint32_t status() const
+    {
+      uint32_t out;
+      if (!m_status_sel) {
+        out = m_tx_fifo.count() < m_status_n ? -1 : 0;
+      } else {
+        out = m_rx_fifo.count() < m_status_n ? -1 : 0;
+      }
+      return out;
+    }
   protected:
+    uint8_t irq_decode(uint8_t val) {
+      uint8_t irqNum = val & 0x07;
+      if (lo5 & 0x10) {
+        irqNum = irqNum & 0x04 + (irqNum + m_id) & 3;
+      }
+      return irqNum;
+    }
   private:
     PIOBlock &m_block;
     PIOTxFifo m_tx_fifo;
@@ -379,6 +506,7 @@ namespace RP2040::PIO{
     uint8_t m_out_base, m_set_base, m_sideset_base, m_in_base;
     uint8_t m_out_count, m_set_count, m_sideset_count;  
     uint8_t m_jmp_pin;
+    uint8_t m_status_n;
     // 0: shift left, 1: shift right
     bool m_in_shiftdir, m_out_shiftdir;
     bool m_autopull, m_autopush;
